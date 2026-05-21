2b7f87cec07cea9d5e32727ea01d9e5e5be0e5db
mannj5 <jcmann67@gmail.com>
2026-05-22 07:30:18 +1000

    Day 21 part 1

---
+++ 2024/day21/keypad.cpp
@@ -0,0 +1,263 @@
+
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <map>
+#include <climits>
+
+using namespace std;
+
+/*
+enum Direction {
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT
+};
+
+// permissions for moving
+// 4 bit number a3     a2      a1     a0
+// means     up ^ down ^ right ^ left ^
+// encode permissions into the array
+
++---+---+---+
+| 7 | 8 | 9 |
++---+---+---+
+| 4 | 5 | 6 |
++---+---+---+
+| 1 | 2 | 3 |
++---+---+---+
+    | 0 | A |
+    +---+---+
+const int keys[4][3]
+    =  {{0b0110, 0b1110, 0b1010},
+        {0b0111, 0b1111, 0b1011},
+        {0b0101, 0b1111, 0b1011},
+        {0,      0b0101, 0b1001}};
+
+    +---+---+
+    | ^ | A |
++---+---+---+
+| < | v | > |
++---+---+---+
+const int arrows[2][3]
+    =  {{0,      0b0110, 0b1010},
+        {0b0100, 0b1101, 0b1001}};
+
+// checks if that bit is set in the permissions
+int can_move_keys(int x, int y, int dir) {
+    return (keys[y][x] & (1 << dir)) > 0;
+}
+
+// checks if that bit is set in the permissions
+int can_move_arrows(int x, int y, int dir) {
+    return (arrows[y][x] & (1 << dir)) > 0;
+}
+
+map<char, pair<int, int>> positions
+    =  {{'7', {0, 0}}, {'8', {1, 0}}, {'9', {2, 0}},
+        {'4', {0, 1}}, {'5', {1, 1}}, {'6', {2, 1}},
+        {'1', {0, 2}}, {'2', {1, 2}}, {'3', {2, 2}},
+        {' ', {0, 3}}, {'0', {1, 3}}, {'A', {2, 3}}};
+*/
+
+/*
++---+---+---+
+| 7 | 8 | 9 |
++---+---+---+
+| 4 | 5 | 6 |
++---+---+---+
+| 1 | 2 | 3 |
++---+---+---+
+    | 0 | A |
+    +---+---+
+*/
+
+const char keypos[4][3] =
+    {{'7', '8', '9'},
+     {'4', '5', '6'},
+     {'1', '2', '3'},
+     {' ', '0', 'A'}};
+
+const char arrowpos[2][3] =
+    {{' ', '^', 'A'},
+     {'<', 'v', '>'}};
+
+map<char, int> int_values =
+    {{' ', 0}, {'^', 1}, {'A', 2},
+    {'<', 3}, {'v', 4}, {'>', 5}};
+
+map<pair<char, char>, vector<string>> keypaths;
+map<pair<char, char>, vector<string>> arrowpaths;
+long long memo[4][6][6] = {0};
+
+vector<string> get_num_path(int sx, int sy, int ex, int ey) {
+    int dx = ex - sx;
+    int dy = ey - sy;
+
+    string horiz, vert;
+    if (dy > 0) {
+        vert = string(dy, 'v');
+    } else {
+        vert = string(-dy, '^');
+    }
+    if (dx > 0) {
+        horiz = string(dx, '>');
+    } else {
+        horiz = string(-dx, '<');
+    }
+
+    vector<string> vec;
+
+    // Pass for numpad
+
+    // if it takes you through the empty space, skip
+    if (dx != 0 && keypos[sy][sx + dx] != ' ') {
+        vec.push_back(horiz + vert + "A");
+    }
+    if (dy != 0 && keypos[sy + dy][sx] != ' ') {
+        vec.push_back(vert + horiz + "A");
+    }
+
+    return vec;
+}
+
+vector<string> get_arrow_path(int sx, int sy, int ex, int ey) {
+    int dx = ex - sx;
+    int dy = ey - sy;
+
+    string horiz, vert;
+    if (dy > 0) {
+        vert = string(dy, 'v');
+    } else {
+        vert = string(-dy, '^');
+    }
+    if (dx > 0) {
+        horiz = string(dx, '>');
+    } else {
+        horiz = string(-dx, '<');
+    }
+
+    vector<string> vec;
+
+    // Pass for numpad
+
+    // if it takes you through the empty space, skip
+    if (dx != 0 && arrowpos[sy][sx + dx] != ' ') {
+        vec.push_back(horiz + vert + "A");
+    }
+    if (dy != 0 && arrowpos[sy + dy][sx] != ' ') {
+        vec.push_back(vert + horiz + "A");
+    }
+
+    return vec;
+}
+
+void build_paths(void) {
+    for (int sy = 0; sy < 4; sy++) {
+        for (int sx = 0; sx < 3; sx++) {
+
+            for (int ey = 0; ey < 4; ey++) {
+                for (int ex = 0; ex < 3; ex++) {
+                    vector<string> val = get_num_path(sx, sy, ex, ey);
+                    pair<char, char> key = make_pair(keypos[sy][sx], keypos[ey][ex]);
+                    keypaths[key] = val;
+
+                    if (sy > 1 || ey > 1) {
+                        continue;
+                    }
+                    val = get_arrow_path(sx, sy, ex, ey);
+                    key = make_pair(arrowpos[sy][sx], arrowpos[ey][ex]);
+                    arrowpaths[key] = val;
+                }
+            }
+        }
+    }
+}
+
+long long solve_state(int depth, char src, char dst) {
+    if (src == dst) {
+        cout << 'A';
+        return 1;
+    }
+
+    // cout << "starting at depth " << depth << endl;
+    int srcix = int_values[src];
+    int dstix = int_values[dst];
+    int min = memo[depth][srcix][dstix];
+    if (min != 0) {
+        return min;
+    }
+
+    min = INT_MAX;
+    if (depth == 0) {
+        /* this is throwing an error because there is no paths for arrow keys */
+        string str = arrowpaths.at({src, dst}).at(0);
+        int min = str.length();
+        cout << str;
+        memo[depth][srcix][dstix] = min;
+        return min;
+    }
+
+    int total;
+    char prev = 'A';
+    for (auto &str : arrowpaths.at({src, dst})) {
+        // cout << str << endl;
+        total = 0;
+        for (auto &c : str) {
+            // cout << "going one level deeper\n";
+            total += solve_state(depth - 1, prev, c);
+            prev = c;
+        }
+        if (total < min) {
+            min = total;
+        }
+    }
+    //
+    memo[depth][srcix][dstix] = min;
+    return min;
+}
+
+long long first_state(char src, char dst) {
+    int total;
+    int min = INT_MAX;
+    char prev = 'A';
+    for (auto &str : keypaths.at({src, dst})) {
+        // cout << str << endl;
+        total = 0;
+        for (auto &c : str) {
+            // cout << "going one level deeper\n";
+            total += solve_state(20, prev, c);
+            prev = c;
+        }
+        if (total < min) {
+            min = total;
+        }
+    }
+    return min;
+}
+
+int main() {
+    // char arr[5][5] = {"029A", "980A", "179A", "456A", "379A"};
+    char arr[5][5] = {"129A", "176A", "169A", "805A", "208A"};
+    build_paths();
+    long long total = 0;
+    for (int i = 0; i < 5; i++) {
+        long long complexity = 0;
+        char prev = 'A';
+        char* ex = arr[i];
+        while (*ex) {
+            complexity += first_state(prev, *ex);
+            prev = *ex;
+            ex++;
+        }
+        ex -= 4;
+        ex[3] = '\0';
+
+        total += complexity * atoi(ex);
+        
+    }
+    printf("\ngot total = %lld\n", total);
+
+    return 0;
+}
